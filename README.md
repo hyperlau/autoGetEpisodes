7776fad2def9b535a76c409de809a85d59a67fac
hyperlau <lo_ol@live.cn>
2020-03-07 14:35:07 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,96 @@
+# autoGetEpisodes
+自动跟踪并下载美剧资源
+
+##
+基于Python3编写，从https://txmeiju.com 搜索关键词 ，筛选出剧集的magnet链接，调用aria2下载资源
+后续会添加邮件通知功能
+代码比较简单，根据配置文件生成剧集的缓存文件，然后根据缓存文件来下载剧集
+
+** 建议跑在NAS的Ubuntu Docker里 ,添加cron任务定时执行 **  
+docker设置的建议:  
+映射一个nas共享目录给docker，用来存放下载的剧集  
+安装好aria2，这里略过  
+建议安装一个aria2前端方便查看任务，推荐AriaNG  https://github.com/mayswind/AriaNg  
+然后以Daemon形式运行aria2
+把docker的  
+22端口 (ssh)
+80端口（查看前端页面）、  
+6800端口（aria2 rpc）  
+6901端口（BT监听端口，在aria2配置文件里修改）  
+6902端口(DHT监听端口，在aria2配置文件里修改)  
+这些端口映射出来，保证你的电脑能访问到  
+6901、6902要映射到公网，且内外端口要一致，否则aria2没有下载速度
+另外自己要有公网IP，到路由器上看你的公网IP和www.ip138.com 上看到的是不是一致，如果不一致的话就请打电话给你的运营商，否则下载还是会没速度  
+~~”您好，我家宽带没有公网IP，但我需要远程查看家里的视频监控设备，请帮我分配一个“~~
+
+## INSTALL
+git clone https://github.com/hyperlau/autoGetEpisodes.git
+
+## HOW TO USE
+
+首先编辑脚本修改配置文件路径  
+    self.configFile=os.path.abspath('/usr/local/autoGetEpisodes/config.cfg')  
+    
+需要下载的剧集信息要写进配置文件里
+
+    [global]
+    #DO NOT CHANGE baseUrl and searchUrl
+    baseUrl=https://txmeiju.com
+    searchUrl=%(baseUrl)s/tv/search?s=
+    # aria2
+    aria2Url=http://localhost:6800/rpc
+    aria2Token=0sdafujlk23nfdsau90coi2jl3knm823oihjfsdnf
+    # download dir
+    downloadDir=/autoGetEpisodes
+    # cache
+    cacheDir=/usr/local/autoGetEpisodes/cache
+    # 抓取页面timeout
+    requestTimeout=10
+    # 抓取sleep时长 防止被封禁IP
+    requestSleep=1
+
+    # 日志
+    logFile=/usr/local/autoGetEpisodes/log.txt
+    logLevel=DEBUG
+
+    [国土安全]
+    # 用于搜索关键词
+    keyWord_0=Homeland.S08
+    # 用于筛选搜索结果的关键词
+    keyWord_1=
+
+    [实习医生格蕾]
+    keyWord_0=Greys.Anatomy.S16
+    keyWord_1=720P
+
+
+首先自己要在网站上手动测试好要下载的剧集关键字（搜索框里输入的关键字）（配置文件里的keyword_0）  
+例如：国土安全  
+因为txmeiju的搜索仅支持连续字符的搜索，所以脚本支持二次筛选搜索结果  (配置文件里的keyword_1)  
+例如: 1080P  
+然后就可以生成缓存文件了  
+    # python3 autoGetEpisodes.py -g  
+    
+缓存文件默认在cache目录下
+因为搜索出的剧集列表可能有我们不想下载的，例如这一季我看到了第10集，那可以编辑缓存文件把不需要下载的剧集的skip改成yes  
+这样就不会被添加到aria2里  
+再运行：
+    # python3 autoGetEpisodes.py -c  
+
+就会开始下载  
+以后每次运行-c参数就可以了  
+添加到aria2的文件的skip标志会自动置为yes
+当有新的剧集的时候就会只添加新的剧集到aria2里
+
+# 关于cron定时执行
+    22 * * * * export LANG='C.UTF-8'; /usr/local/autoGetEpisodes/autoGetEpisodes.py -c  
+因为脚本里有中文，配置文件里也有中文，脚本在终端下执行没问题，但到了crontab里就会报错，所以要加export LANG='C.UTF-8';
+
+回头补一个ubuntu docker配置的小文档，里面会有中文相关配置，供大家参考
+
+简单吧？
+
+
+# 注意事项：  
+1 如果生成缓存后修改了配置文件里已经存在的剧集配置的keyword，需要删除对应的缓存文件重新生成  
+2 配置文件里的路径要用绝对路径
---
+++ autoGetEpisodes.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import time
+import getopt
+import os
+import sys
+import re
+import logging
+import configparser
+import requests
+from requests.adapters import HTTPAdapter
+from bs4 import BeautifulSoup
+from pyaria2 import Aria2RPC
+
+# 读配置文件
+class GetConfig(object):
+    def __init__(self):
+        self.configFile=os.path.abspath('/usr/local/autoGetEpisodes/config.cfg')
+       
+    def getConfig(self):
+        # 读取配置文件
+        print('Load Config...')
+        config=configparser.ConfigParser()
+        config.read(self.configFile)
+        config.episodesList=[]
+        # 找出配置文件里的剧集配置
+        episodesConfigSections=config.sections()
+        config.episodesList=[i for i in episodesConfigSections if i !='global'];
+        return(config)
+
+
+# 初始化日志
+class logInit(object):
+    def __init__(self,config):
+        logging.basicConfig(
+            level=eval('logging.'+config.get('global','logLevel')),
+            format="%(asctime)s---%(lineno)s----%(name)s: %(message)s",
+            filename=os.path.abspath(config.get('global','logFile')),
+            filemode="a"
+        )
+
+
+
+# 根据配置文件来抓取url，写入缓存文件
+class prepareCacheFile(object):
+    def __init__(self,config):
+        self.config=config
+        self.baseUrl=self.config.get('global','baseUrl')
+        self.searchUrl=self.config.get('global','searchUrl')
+        self.cacheDir=os.path.abspath(self.config.get('global','cacheDir'))
+        self.requestTimeout=self.config.getint('global','requestTimeout')
+        self.requestSleep=self.config.getint('global','requestSleep')
+        # 创建cache目录
+        cacheDirPath=os.path.abspath(self.cacheDir)
+        if not os.path.exists(cacheDirPath):
+            os.mkdir(cacheDirPath) 
+            logging.debug('创建缓存目录 '+cacheDirPath+' 成功!')
+        else:
+            logging.debug('找到缓存目录'+cacheDirPath)
+                # 根据剧集列表创建缓存文件，后面直接读取，不再做判断     
+        for i in self.config.episodesList:
+            episodeCacheFile=self.getCacheFileAbsPath(i)
+            if not os.path.exists(episodeCacheFile):
+                with open(episodeCacheFile,'w') as f:
+                    logging.debug("创建缓存文件 "+f.name+' 成功!')
+            else:
+                logging.debug('找到缓存文件'+episodeCacheFile)
+
+    # 返回缓存文件绝对路径
+    def getCacheFileAbsPath(self,episodeName):
+        return(self.cacheDir+'/'+episodeName+'.cache')
+
+ 
+    # 获取搜索结果,然后筛选所有符合keyword_1要求的magnetUrl
+    def getEpisodesUrls(self,searchUrl,episodesName,keyWord_1):
+        head={}
+        head['User-Agent']='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15'
+        # 访问搜索URL得到剧集URL列表
+        s=requests.Session()
+        s.mount('http://', HTTPAdapter(max_retries=3))
+        s.mount('https://', HTTPAdapter(max_retries=3))
+        try:
+            time.sleep(self.requestSleep)
+            req=s.get(searchUrl,headers=head,timeout=self.requestTimeout)
+            # 错误就抛出异常
+            req.raise_for_status()
+        except IOError as e:
+            logging.error('一级抓取错误：',e)
+            exit(1)
+        html=req.text
+        bf=BeautifulSoup(html,'html.parser')
+        texts=bf.find_all('a',class_='detail-url')
+        # 进入剧集页面抓取magnet链接
+        results=[]
+        for i in texts:
+            episodePage=self.baseUrl+'/'+i['href']
+            try:
+                time.sleep(self.requestSleep)
+                reqEpisodesPage=requests.get(episodePage,headers=head,timeout=self.requestTimeout)
+                # 错误就抛出异常
+                reqEpisodesPage.raise_for_status()
+            except IOError as e:
+                logging.error('二级抓取错误：',e)
+                exit(1)
+            epHtml=reqEpisodesPage.text
+            epBf=BeautifulSoup(epHtml,'html.parser')
+            epTexts=epBf.find('a',class_='mag-url')
+            # 根据keyword_1筛选链接
+            if re.search(keyWord_1,i.span.string):
+                results.append({
+                   # 'name':episodesName,
+                    'magUrl':epTexts['href'],
+                    'fileName':i.span.string
+                })
+        return(results)
+    
+    # 遍历剧集列表，抓取剧集magnetURL和文件名,写入缓存文件
+    def writeCacheFile(self):
+        for i in self.config.episodesList:
+            cacheFile=self.getCacheFileAbsPath(i)
+            keyword_0=self.config.get(i,'keyWord_0')
+            keyword_1=self.config.get(i,'keyWord_1')
+            # 抓取剧集magnetURL和文件名
+            searchURL=self.searchUrl+keyword_0
+            logging.info('开始抓取 '+i)
+            logging.info('--searchURL='+searchURL)
+            logging.info('--筛选关键词：'+keyword_1)
+            episodeInfo=self.getEpisodesUrls(searchURL,i,keyword_1)
+            epiCache=configparser.ConfigParser()
+            # 写入缓存文件
+            # 前面初始化的时候已经创建了缓存文件，所以直接read
+            cacheFileName=self.getCacheFileAbsPath(i)
+            epiCache.read(cacheFileName)
+            for j in episodeInfo:
+                logging.debug(j)
+                # 跳过存在的配置
+                if epiCache.has_section(j['fileName']):
+                    logging.info('['+j['fileName']+'] 已经存在，skip...')
+                    continue
+                epiCache[j['fileName']]={
+                        'magnetURL':j['magUrl'],
+                        'skip':'no'
+                        }
+                logging.info('['+j['fileName']+'] 写入：')
+                logging.info('magnetURL='+j['magUrl'])
+                logging.info('skip=no')
+            
+            with open(cacheFileName,'w') as cacheFile:
+                epiCache.write(cacheFile)
+
+
+# 使用Aria2下载剧集
+class GetEpisodes(object):
+
+    def __init__(self,config):
+        self.config=config
+        # 初始化 aria2 rpc
+        logging.info('初始化 aria2 rpc')
+        self.aria2Token=self.config.get('global','aria2Token')
+        self.aria2Url=self.config.get('global','aria2Url')
+        self.downloadDir=os.path.abspath(self.config.get('global','downloadDir'))
+        self.jsonrpc=Aria2RPC(url=self.aria2Url,token=self.aria2Token)
+        self.jsonrpcOptions = {
+            'dir': self.downloadDir
+        }
+
+    def download(self,name,url):
+        self.jsonrpcOptions['out']=name
+        jsonrpcReturn = self.jsonrpc.addUri(
+            [url],
+            options=self.jsonrpcOptions
+        )
+        return(jsonrpcReturn)
+
+    def readCacheFile(self):
+        self.downloadList=[]
+        for i in self.config.episodesList:
+            cacheFilePath=os.path.abspath(self.config.get('global','cacheDir')+'/'+i+'.cache')
+            logging.debug('读取缓存文件'+cacheFilePath)
+            cacheFile=configparser.ConfigParser()
+            cacheFile.read(cacheFilePath)
+            for s in cacheFile.sections():
+                # 跳过skip的文件
+                if cacheFile.getboolean(s,'skip') != True:
+                    magUrl=cacheFile.get(s,'magneturl')
+                    self.downloadList.append({
+                        'fileName':s,
+                        'magUrl':magUrl
+                        })
+                    logging.debug('添加到下载列表：\nFileName：'+s+'\nmagUrl:'+magUrl)
+                    # 修改skip为True
+                    cacheFile.set(s,'skip','yes')
+            with open(cacheFilePath,'w') as f:
+                cacheFile.write(f)
+
+
+        logging.debug('下载列表共包含如下 ' + str(len(self.downloadList))+' 条记录：')
+        for j in self.downloadList:
+            logging.debug(j)
+
+
+    def downloadFiles(self):
+        self.readCacheFile()
+        for i in self.downloadList:
+            logging.info('添加下载任务：'+i['fileName']+'\nmagnetURL:'+i['magUrl'])
+            aria2DownloadID=self.download(i['fileName'],i['magUrl'])
+            logging.info('Aria2任务ID：'+aria2DownloadID)
+
+
+#class SendEmail(object):
+
+
+if __name__=='__main__':
+    if len(sys.argv)<2:
+        print('usage:')
+        print(sys.argv[0]+' -g     just only generate cache file')
+        print(sys.argv[0]+' -c     generate cache file and get episodes')
+        sys.exit(2)
+    if sys.argv[1] == '-g':
+        config=GetConfig().getConfig()
+        logInit(config)
+        print('Please Run " tail -f '+os.path.abspath(config.get('global','logFile'))+' " to view the log...')
+        logging.info('===开始准备缓存文件===')
+        print('===开始准备缓存文件===')
+        prepareCacheFile(config).writeCacheFile()
+        logging.info('===缓存文件准备完成===')
+        print('===缓存文件准备完成===')
+        
+    elif sys.argv[1] == '-c':
+        config=GetConfig().getConfig()
+        logInit(config)
+        print('Please Run " tail -f '+os.path.abspath(config.get('global','logFile'))+' " to view the log...')
+        logging.info('===开始准备缓存文件===')
+        print('===开始准备缓存文件===')
+        prepareCacheFile(config).writeCacheFile()
+        logging.info('===缓存文件准备完成===')
+        print('===缓存文件准备完成===')
+        print('===添加下载任务===')
+        g=GetEpisodes(config)
+        g.downloadFiles()
+        print('===添加下载任务完成===')
+
+
+    else:
+        print('usage:')
+        print(sys.argv[0]+' -g     just only generate cache file')
+        print(sys.argv[0]+' -c     generate cache file and get episodes')
+        sys.exit(2)
+ 
